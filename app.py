96f739b5205adc867a3c53ecf3003fa454c05e82
Masaaki <narita.masaaki@cloud-ace.jp>
2024-11-18 19:18:14 +0900

    ウィンドウサイズを指定

--- app.py
+++ app.py
@@ -42,6 +42,7 @@ def home():
         chrome_options.add_argument("--headless")
         chrome_options.add_argument("--no-sandbox")
         chrome_options.add_argument("--disable-dev-shm-usage")
+        chrome_options.add_argument("--window-size=1920,1080")  #ウィンドウサイズを固定
 
         # ChromeのWebDriverを起動
         screenshot_path = "screenshot.png"
